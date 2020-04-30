5e867d5fc4d34e4a997c671144947dbd50428757
Stan Drozd <drozdziak1@gmail.com>
2020-04-30 14:33:31 +0200

    Move on to async

--- src/main.rs
+++ src/main.rs
@@ -3,72 +3,75 @@ extern crate reqwest;
 extern crate futures;
 extern crate scraper;
 
+use futures::future;
 use scraper::{Html, Selector};
+use tokio::process::Command;
 
-use std::io::{self, Write};
-use std::process::Command;
+use std::iter::Iterator;
 
 const URL: &'static str = "https://www.musicforprogramming.net";
 
-pub fn main() {
-    let mut resp = reqwest::get(URL).unwrap();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get(URL).await?;
     if !resp.status().is_success() {
         panic!("Request failed for {}", URL);
     }
 
-    let body = resp.text().unwrap();
+    let body = resp.text().await?;
 
     let fragment = Html::parse_document(&body);
 
-    let ep_selec = Selector::parse("#episodes a").expect("Couldn't parse the episode selector");
     let file_selec =
         Selector::parse("div .pad a[href$=mp3]").expect("Couldn't parse the file link selector");
 
-    let mut file_links: Vec<String> = Vec::new();
-
-    for episode in fragment.select(&ep_selec) {
-        let subpage = episode.value().attr("href").unwrap();
-        let ep_url = &format!("{}{}{}", URL, "/", subpage);
-        let mut subpage_resp = reqwest::get(ep_url).unwrap();
-        if !subpage_resp.status().is_success() {
-            panic!("Request failed for {}", URL);
-        }
-
-        let subpage_body = subpage_resp.text().unwrap();
-
-        let subpage_fragment = Html::parse_document(&subpage_body);
-
-        let file_url = subpage_fragment
-            .select(&file_selec)
-            .nth(0)
-            .expect(&format!("Couldn't find file URL in {}", ep_url))
-            .value()
-            .attr("href")
-            .expect(&format!("Failed to get href at {}", ep_url));
-
-        eprintln!("file_url: {}", file_url);
-        file_links.push(String::from(file_url));
-    }
-
-    let latest = fragment
+    let latest_url = fragment
         .select(&file_selec)
         .nth(0)
-        .expect(&format!("Couldn't find file URL in {}", URL))
+        .expect("Couldn't find latest URL")
         .value()
         .attr("href")
-        .expect(&format!("Failed to get href at {}", URL));
+        .expect(&format!("Failed to get href for latest"));
 
-    eprintln!("latest: {}", latest);
-    file_links.push(String::from(latest));
+    let fname = latest_url.split("/").last().unwrap();
+    println!("Downloading {}... ", fname);
 
+    Command::new("wget").args(&["-N", &latest_url]).output().await?;
+    println!("{} OK", fname);
 
-    for link in file_links {
-        let fname = link.split("/").last().unwrap();
-        print!("Downloading {}... ", fname);
-        io::stdout().flush().unwrap();
+    let ep_selec = Selector::parse("#episodes a").expect("Couldn't parse the episode selector");
+    let futs = fragment
+        .select(&ep_selec)
+        .enumerate()
+        .map(|(idx, episode)| async move {
+            let file_selec = Selector::parse("div .pad a[href$=mp3]")
+                .expect("Couldn't parse the file link selector");
 
-        Command::new("wget").args(&["-N", &link]).output().expect("Download failed.");
-        println!("OK");
-    }
+            let subpage = episode.value().attr("href").unwrap();
+            let ep_url = &format!("{}/{}", URL, subpage);
+            let subpage_resp = reqwest::get(ep_url).await?;
 
+            let body = subpage_resp.text().await?;
+
+            let subpage_fragment = Html::parse_document(&body);
+
+            let file_url = subpage_fragment
+                .select(&(file_selec.clone()))
+                .nth(0)
+                .expect(&format!("Couldn't find file URL for {}", idx + 1))
+                .value()
+                .attr("href")
+                .expect(&format!("Failed to get href for {}", idx + 1));
+
+            let fname = file_url.split("/").last().unwrap();
+            println!("Downloading {}... ", fname);
+
+            Command::new("wget").args(&["-N", &file_url]).output().await?;
+            println!("{} OK", fname);
+
+            Result::<(), Box<dyn std::error::Error>>::Ok(())
+        });
+
+    future::try_join_all(futs).await?;
+    Ok(())
 }
